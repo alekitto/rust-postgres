b031f31b115d6af7a928ffafbca4867f9682cf19
Alessandro Chitolina <alekitto@gmail.com>
2021-06-17 20:23:50 +0200

    do not close portal if raw object is dropped

--- tokio-postgres/src/raw/portal.rs
+++ tokio-postgres/src/raw/portal.rs
@@ -11,19 +11,6 @@ struct Inner {
     name: String,
 }
 
-impl Drop for Inner {
-    fn drop(&mut self) {
-        if let Some(client) = self.client.upgrade() {
-            let buf = client.with_buf(|buf| {
-                frontend::close(b'P', &self.name, buf).unwrap();
-                frontend::sync(buf);
-                buf.split().freeze()
-            });
-            let _ = client.send(RequestMessages::Single(FrontendMessage::Raw(buf)));
-        }
-    }
-}
-
 /// A portal.
 ///
 /// Portals can only be used with the connection that created them, and only exist for the duration of the transaction
